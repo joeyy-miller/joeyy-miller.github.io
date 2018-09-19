aec32e2667a472f8857db76b017223cf7107086b
joey <joey@Big-Steve.local>
2018-09-19 03:59:11 -0700

    Changed to Console6

---
+++ old/sys/kernel/kernel.js
@@ -0,0 +1,138 @@
+/* Kernel Task
+	Used to run the system*/
+var PANIC = false;
+function kernel_task(data1) {
+	switch (data1) {
+		case "panic":
+			display.consoleWrite("<span class='_f'>A Kernel Panic has occured, please restart your system.</span>");
+			document.getElementById("userInputBar").readOnly = true; 
+			document.getElementById("userInputBar").style.backgroundColor = '#ce2732'; 
+			document.getElementById("powerButton").style.backgroundColor = '#ce2732'; 
+			document.getElementById("time").id = "brk-time";
+			PANIC = true;
+			break;
+		case "unpanic":
+			if (PANIC == true) {
+				document.getElementById("userInputBar").readOnly = false; 
+				document.getElementById("userInputBar").style.backgroundColor = '#FFF'; 
+				document.getElementById("powerButton").style.backgroundColor = '#000'; 
+				document.getElementById("brk-time").id = "time";
+				PANIC = false;
+			}
+			break;
+		case "display":
+			consoleDisplay(); 
+			break;
+		case "power":
+			sys.power();
+			break;
+	}
+	return "ok";
+}
+
+var pass = 0;
+var position = 0;
+function animateMan() {
+	if (pass == 0) {
+		position = display.linenumber;
+		display.linenumber += 3;
+		display.consoleline += 3;
+		pass = 15;
+	} 
+	if (pass == 15) {
+		display.lineContents[position-1] = "  o  ";
+		display.lineContents[position] = " /#\\ ";
+		display.lineContents[position+1] = " _|_ ";
+		display.rsh_scrn();
+	} else if (pass == 30) {
+		display.lineContents[position-1] = " \\o/ ";
+		display.lineContents[position] = "  #  ";
+		display.lineContents[position+1] = "_/\\_";
+		display.rsh_scrn();
+		pass = 1;
+	}
+	pass++;
+	if (sys.pwrSts && display.animation) {
+		setTimeout(animateMan, 50);
+	} else {
+		display.clear();
+		pass = 0;
+		position = 0;
+	}
+}
+
+function consoleWrite(data1) {
+	display.consoleWrite(data1);
+}
+/* Enter Check
+   Checks when to submit the user's command
+*/
+function entr_ck(e) {
+    var unicode=e.keyCode? e.keyCode : e.charCode
+    if (unicode == 13) 
+	{ // Enter Key
+		reg_input('display');
+		consolev.pushCommand(document.getElementById("userInputBar").value);
+		document.getElementById("userInputBar").value = "";
+	}
+	if (unicode == 38)
+	{ // Up Arrow
+		if (!rght.focus) 
+			document.getElementById("userInputBar").value = consolev.pullCommandBack();
+		else {
+			rght.upArrow();
+		}
+	}
+	if (unicode == 40)
+	{ // Up Arrow
+		if (!rght.focus) 
+			document.getElementById("userInputBar").value = consolev.pullCommandForward();
+		else {
+			rght.downArrow();
+		}
+	}
+}
+
+/* Keydown Event
+   Listener event that runs whenver a key is pressed to check if an action 
+   	needs to be performed.
+*/
+document.onkeydown = function(evt) {
+	evt = evt || window.event;
+	var isEnter = false;
+	if ("key" in evt) { isEnter = (evt.key == "Enter");
+	} else { isEnter = (evt.keyCode == 13); }
+	if (isEnter) {
+		if (!sys.pwrSts && sys.turnOffFlag) {
+			kernel_task("power");
+		} else if (rght.focus) {
+			rght.enter();
+		}
+	}
+	var isEnter = false;
+	if ("key" in evt) { isText = (evt.key == "t");
+	} else { isText = (evt.keyCode == 84); }
+	if (isText) {
+		if (!(document.getElementById("userInputBar") === document.activeElement)) {
+			document.getElementById("userInputBar").focus();
+			var inputValue = document.getElementById("userInputBar").value;
+			inputValue = inputValue.substring(0,inputValue.length - 1);
+			setTimeout(function() {document.getElementById("userInputBar").value = inputValue;}, 10 );
+		}
+		if (consolev.debugMode) {consoleWrite("Text key.")}
+
+	}
+	var isEscape = false;
+	if ("key" in evt) { isEscape = (evt.key == "Escape" || evt.key == "Esc");
+	} else { isEscape = (evt.keyCode == 27); } 
+	if (isEscape && sys.pwrSts && sys.turnOffFlag) {
+		kernel_task("power");
+	}
+	var isOption = false;
+	if ("key" in evt) { isOption = (evt.key == "Alt" || evt.key == "Option");
+	} else { isOption = (evt.keyCode == 18); } 
+	if (isOption && sys.pwrSts) {
+		rght.setFocus();
+		if (consolev.debugMode) {consoleWrite("Option key.")}
+	}
+}
--- sys/kernel/kernel.js
+++ sys/kernel/kernel.js
@@ -1,28 +1,80 @@
-/* Kernel Task
-	Used to run the system*/
-var PANIC = false;
+
+
+//Make the DIV element draggagle:
+	dragElement(document.getElementById("mydiv"));
+
+	function dragElement(elmnt) {
+	  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+	  if (document.getElementById(elmnt.id + "header")) {
+	    /* if present, the header is where you move the DIV from:*/
+	    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+	  } else {
+	    /* otherwise, move the DIV from anywhere inside the DIV:*/
+	    elmnt.onmousedown = dragMouseDown;
+	  }
+
+	  function dragMouseDown(e) {
+	    e = e || window.event;
+	    e.preventDefault();
+	    // get the mouse cursor position at startup:
+	    pos3 = e.clientX;
+	    pos4 = e.clientY;
+	    document.onmouseup = closeDragElement;
+	    // call a function whenever the cursor moves:
+	    document.onmousemove = elementDrag;
+	  }
+
+	  function elementDrag(e) {
+	    e = e || window.event;
+	    e.preventDefault();
+	    // calculate the new cursor position:
+	    pos1 = pos3 - e.clientX;
+	    pos2 = pos4 - e.clientY;
+	    pos3 = e.clientX;
+	    pos4 = e.clientY;
+	    // set the element's new position:
+	    if ((elmnt.offsetTop - pos2) < 433 && (elmnt.offsetTop - pos2) > 8) {
+	  	  elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+		}
+		if ((elmnt.offsetLeft - pos1) < 460 && (elmnt.offsetLeft - pos1) > 8)
+	    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+
+	    console.log("Position 1:" + pos1 + " Position 2: " + pos2 + " Position 3:" + pos3 + " Position 4:" + pos4);
+	  }
+
+	  function closeDragElement() {
+	    /* stop moving when mouse button is released:*/
+	    document.onmouseup = null;
+	    document.onmousemove = null;
+	  }
+	}
+
+
+function getTime() {
+	var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    if (m < 10) {m = "0" + m};
+    if (s < 10) {s = "0" + s};
+    if (h < 1) {h = "12"} // 0 hours or 12 am
+    if (h > 12) {h-=12;s += " pm"}else{s+=" am"};
+	return h + ":" + m + ":" + s;
+}
+
+function time() {
+    document.getElementById('time').innerHTML = getTime();
+    setTimeout(time, 500);
+}
+
+time();
+/*
+
+
+
+
 function kernel_task(data1) {
 	switch (data1) {
-		case "panic":
-			display.consoleWrite("<span class='_f'>A Kernel Panic has occured, please restart your system.</span>");
-			document.getElementById("userInputBar").readOnly = true; 
-			document.getElementById("userInputBar").style.backgroundColor = '#ce2732'; 
-			document.getElementById("powerButton").style.backgroundColor = '#ce2732'; 
-			document.getElementById("time").id = "brk-time";
-			PANIC = true;
-			break;
-		case "unpanic":
-			if (PANIC == true) {
-				document.getElementById("userInputBar").readOnly = false; 
-				document.getElementById("userInputBar").style.backgroundColor = '#FFF'; 
-				document.getElementById("powerButton").style.backgroundColor = '#000'; 
-				document.getElementById("brk-time").id = "time";
-				PANIC = false;
-			}
-			break;
-		case "display":
-			consoleDisplay(); 
-			break;
 		case "power":
 			sys.power();
 			break;
@@ -30,109 +82,31 @@ function kernel_task(data1) {
 	return "ok";
 }
 
-var pass = 0;
-var position = 0;
-function animateMan() {
-	if (pass == 0) {
-		position = display.linenumber;
-		display.linenumber += 3;
-		display.consoleline += 3;
-		pass = 15;
-	} 
-	if (pass == 15) {
-		display.lineContents[position-1] = "  o  ";
-		display.lineContents[position] = " /#\\ ";
-		display.lineContents[position+1] = " _|_ ";
-		display.rsh_scrn();
-	} else if (pass == 30) {
-		display.lineContents[position-1] = " \\o/ ";
-		display.lineContents[position] = "  #  ";
-		display.lineContents[position+1] = "_/\\_";
-		display.rsh_scrn();
-		pass = 1;
-	}
-	pass++;
-	if (sys.pwrSts && display.animation) {
-		setTimeout(animateMan, 50);
-	} else {
-		display.clear();
-		pass = 0;
-		position = 0;
-	}
-}
 
-function consoleWrite(data1) {
-	display.consoleWrite(data1);
-}
-/* Enter Check
-   Checks when to submit the user's command
-*/
-function entr_ck(e) {
-    var unicode=e.keyCode? e.keyCode : e.charCode
-    if (unicode == 13) 
-	{ // Enter Key
-		reg_input('display');
-		consolev.pushCommand(document.getElementById("userInputBar").value);
-		document.getElementById("userInputBar").value = "";
+/* System Class
+	Used to store system settings and statuses
+*
+class System {
+	constructor() {
+		this.pwrSts = false; // Power off 
+		this.version = "5.3.0a";
+		this.versionShort = "5.3[dev]";
+		this.turnOffFlag = true; // This is used by power function to stop the global 'enter' fucntion from turning the system back on
 	}
-	if (unicode == 38)
-	{ // Up Arrow
-		if (!rght.focus) 
-			document.getElementById("userInputBar").value = consolev.pullCommandBack();
-		else {
-			rght.upArrow();
-		}
-	}
-	if (unicode == 40)
-	{ // Up Arrow
-		if (!rght.focus) 
-			document.getElementById("userInputBar").value = consolev.pullCommandForward();
-		else {
-			rght.downArrow();
+
+	power() {
+		if (this.pwrSts) {
+			// Power off system
+			this.turnOffFlag = false;
+			this.pwrSts = false;
+		} else {
+			document.getElementById("powerButton").style.display = "none";
+			//setTimeout(function() {document.getElementById("powerButton").setAttribute("onclick",'kernel_task("power")'); sys.turnOffFlag = true;}, 450);
+			//Power On System
+			this.pwrSts = true;
+			this.turnOffFlag = false;
 		}
 	}
 }
 
-/* Keydown Event
-   Listener event that runs whenver a key is pressed to check if an action 
-   	needs to be performed.
-*/
-document.onkeydown = function(evt) {
-	evt = evt || window.event;
-	var isEnter = false;
-	if ("key" in evt) { isEnter = (evt.key == "Enter");
-	} else { isEnter = (evt.keyCode == 13); }
-	if (isEnter) {
-		if (!sys.pwrSts && sys.turnOffFlag) {
-			kernel_task("power");
-		} else if (rght.focus) {
-			rght.enter();
-		}
-	}
-	var isEnter = false;
-	if ("key" in evt) { isText = (evt.key == "t");
-	} else { isText = (evt.keyCode == 84); }
-	if (isText) {
-		if (!(document.getElementById("userInputBar") === document.activeElement)) {
-			document.getElementById("userInputBar").focus();
-			var inputValue = document.getElementById("userInputBar").value;
-			inputValue = inputValue.substring(0,inputValue.length - 1);
-			setTimeout(function() {document.getElementById("userInputBar").value = inputValue;}, 10 );
-		}
-		if (consolev.debugMode) {consoleWrite("Text key.")}
-
-	}
-	var isEscape = false;
-	if ("key" in evt) { isEscape = (evt.key == "Escape" || evt.key == "Esc");
-	} else { isEscape = (evt.keyCode == 27); } 
-	if (isEscape && sys.pwrSts && sys.turnOffFlag) {
-		kernel_task("power");
-	}
-	var isOption = false;
-	if ("key" in evt) { isOption = (evt.key == "Alt" || evt.key == "Option");
-	} else { isOption = (evt.keyCode == 18); } 
-	if (isOption && sys.pwrSts) {
-		rght.setFocus();
-		if (consolev.debugMode) {consoleWrite("Option key.")}
-	}
-}
+var sys = new System();*/
