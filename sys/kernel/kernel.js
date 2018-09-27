609e225a6d87feb08a7e9bd49865d1659c9e1d4f
joey <joey@Big-Steve.local>
2018-09-26 22:46:28 -0700

    Updated Kernel (0.2a)

--- sys/kernel/kernel.js
+++ sys/kernel/kernel.js
@@ -1,218 +1,30 @@
-//Make the DIV element draggagle:
-/*
-	dragElement(document.getElementById("mydiv"));
+/*_______/\\\\\__________/\\\\\\\\\\\__________________/\\\____________/\\\\\_        
+  _____/\\\///\\\______/\\\/////////\\\______________/\\\/_________/\\\\////__       
+   ___/\\\/__\///\\\___\//\\\______\///_____________/\\\/________/\\\///_______      
+    __/\\\______\//\\\___\////\\\__________________/\\\/________/\\\\\\\\\\\____     
+     _\/\\\_______\/\\\______\////\\\_____________/\\\/_________/\\\\///////\\\__    
+      _\//\\\______/\\\__________\////\\\________/\\\/__________\/\\\______\//\\\_   
+       __\///\\\__/\\\_____/\\\______\//\\\_____/\\\/____________\//\\\______/\\\__  
+        ____\///\\\\\/_____\///\\\\\\\\\\\/____/\\\/_______________\///\\\\\\\\\/___ 
+         ______\/////_________\///////////_____\///___________________\/////////_____
 
-	function dragElement(elmnt) {
-	  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-	  if (document.getElementById(elmnt.id + "header")) {
-	    /* if present, the header is where you move the DIV from:*
-	    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-	  } else {
-	    /* otherwise, move the DIV from anywhere inside the DIV:*
-	    elmnt.onmousedown = dragMouseDown;
-	  }
-
-	  function dragMouseDown(e) {
-	    e = e || window.event;
-	    e.preventDefault();
-	    // get the mouse cursor position at startup:
-	    pos3 = e.clientX;
-	    pos4 = e.clientY;
-	    document.onmouseup = closeDragElement;
-	    // call a function whenever the cursor moves:
-	    document.onmousemove = elementDrag;
-	  }
-
-	  function elementDrag(e) {
-	    e = e || window.event;
-	    e.preventDefault();
-	    // calculate the new cursor position:
-	    pos1 = pos3 - e.clientX;
-	    pos2 = pos4 - e.clientY;
-	    pos3 = e.clientX;
-	    pos4 = e.clientY;
-	    // set the element's new position:
-	    if ((elmnt.offsetTop - pos2) < 433 && (elmnt.offsetTop - pos2) > 8) {
-	  	  elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-		}
-		if ((elmnt.offsetLeft - pos1) < 460 && (elmnt.offsetLeft - pos1) > 8)
-	    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-
-	    console.log("Position 1:" + pos1 + " Position 2: " + pos2 + " Position 3:" + pos3 + " Position 4:" + pos4);
-	  }
-
-	  function closeDragElement() {
-	    /* stop moving when mouse button is released:*
-	    document.onmouseup = null;
-	    document.onmousemove = null;
-	  }
-	}
-
-	dragElement2(document.getElementById("term"));
-
-	function dragElement2(elmnt) {
-	  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-	  if (document.getElementById(elmnt.id + "header")) {
-	    /* if present, the header is where you move the DIV from:*
-	    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-	  } else {
-	    /* otherwise, move the DIV from anywhere inside the DIV:*
-	    elmnt.onmousedown = dragMouseDown;
-	  }
-
-	  function dragMouseDown(e) {
-	    e = e || window.event;
-	    e.preventDefault();
-	    // get the mouse cursor position at startup:
-	    pos3 = e.clientX;
-	    pos4 = e.clientY;
-	    document.onmouseup = closeDragElement2;
-	    // call a function whenever the cursor moves:
-	    document.onmousemove = elementDrag;
-	  }
-
-	  function elementDrag(e) {
-	    e = e || window.event;
-	    e.preventDefault();
-	    // calculate the new cursor position:
-	    pos1 = pos3 - e.clientX;
-	    pos2 = pos4 - e.clientY;
-	    pos3 = e.clientX;
-	    pos4 = e.clientY;
-	    // set the element's new position:
-	    if ((elmnt.offsetTop - pos2) < 433 && (elmnt.offsetTop - pos2) > 8) {
-	  	  elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-		}
-		if ((elmnt.offsetLeft - pos1) < 460 && (elmnt.offsetLeft - pos1) > 8)
-	    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-
-	    console.log("Position 1:" + pos1 + " Position 2: " + pos2 + " Position 3:" + pos3 + " Position 4:" + pos4);
-	  }
-
-	  function closeDragElement2() {
-	    /* stop moving when mouse button is released:*
-	    document.onmouseup = null;
-	    document.onmousemove = null;
-	  }
-	}
-
-
-function getTime() {
-	var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    if (m < 10) {m = "0" + m};
-    if (s < 10) {s = "0" + s};
-    if (h < 1) {h = "12"} // 0 hours or 12 am
-    if (h > 12) {h-=12;s += " pm"}else{s+=" am"};
-	return h + ":" + m + ":" + s;
-}
-
-function time() {
-    document.getElementById('time').innerHTML = getTime();
-    setTimeout(time, 500);
-}
-
-time();
-
-var open = true;
-var term = false;
-var safee = true;
-var hamm = false;
-var power = true;
-function welcome() {
-	if (open == true) {
-		document.getElementById("mydiv").style.display = "none";
-		open = false;
-
-	} else {
-		document.getElementById("mydiv").style.display = "block";
-		open = true;
-	}
-
-}
-function ham() {
-	if (hamm == true) {
-		document.getElementById("drop-down").style.display = "none";
-		hamm = false;
-
-	} else {
-		document.getElementById("drop-down").style.display = "block";
-		hamm = true;
-	}
-
-}
-function terminal() {
-	if (term == true) {
-		document.getElementById("term").classList.add("hidden");
-		term = false;
-
-	} else {
-		document.getElementById("term").classList.remove("hidden");
-		term = true;
-	}
-}
-
-function safe() {
-	if (safee == true) {
-	document.getElementById("topBar").style.background = "#00A635";  
-	document.getElementById("hamburger").style.background = "#00A635";  
-	document.getElementById("ctitle").innerHTML = "Console OS 6 [SAFE MODE]";  
-	safee = false;
-
-	} else {
-	document.getElementById("topBar").style.background = "#272822";
-	document.getElementById("hamburger").style.background = "#272822";
-	document.getElementById("ctitle").innerHTML = "Console OS";  
-		safee = true;
-	}
-}
-function shutdown () {
-	if (power == true) {
-		document.getElementById("screen").style.display = "none";
-		document.getElementById("pwr-btn").style.display = "block";
-		power=false;
-	} else {
-		document.getElementById("screen").style.display = "block";
-		document.getElementById("pwr-btn").style.display = "none";
-		power=true;
-	}
-}
+    (c)2018 iJD Software. MIT License. Open-Source. Welcome to Console 6. GUI. 
 */
 
-/* Example Object 
-let obj = (function(){
-    let _prop1 = 'Starman';
-    return {
-        prop2: 1981,
-        get prop1(){
-            return 'Movie:' + _prop1;
-        },
-        set prop1(_val){
-            _prop1 = _val;
-        }
-    }
-})();
-
-for(let prop in obj){
-    console.log(prop);
-}
-console.log( obj.prop2 ); //1981
-console.log( obj.prop1 ); //Starman
-obj.prop1 = 'The Big Lebowski';
-console.log( obj.prop1 );
-console.log( obj._prop1);
-
-*/
+/*****************************
+    K e r n e l   S p a c e
+*****************************/
 
 /**************************
-  Lightening  Kernel 0.1
+  Lightening  Kernel 1.0,3
 **************************/
 let kernel = (function() {
 
 	// Private Variables
-	let _power = false; // System power status. false = off, true = on.
+		// Private Variables
+	let _background = "", // Path to background.
+		_boot_bg = "",
+	    _power = false; // System power status. false = off, true = on.
 
 	return {
 
@@ -220,23 +32,52 @@ let kernel = (function() {
 		version : '0.1x', // kernel version
 
 		// Get functions
-		get power() { /* Turns system on or off */
+		get power() 
+		{ /* Turns system on or off */
 			if(_power == false) { // System off, so turn on.
 
-				_power = true;
-				document.getElementById("indicator").style.color = "#9ed62a";
-				display.boot;
+				this.boot;
 				
 			} else { // System is on, turn off.
 
-				_power = false;
-				document.getElementById("indicator").style.color = "#ce2732";
-				display.clear;
+				this.shutdown;
 
 			}
 		},
 
+		get boot() {
+			_power = true;
+			document.getElementById("indicator").style.color = "#9ed62a";
+			sys.boot;
+		},
+
+		get shutdown() {
+			_power = false;
+			document.getElementById("indicator").style.color = "#ce2732";
+			sys.clear;
+		},
+
+		set backColor(color_) 
+		{
+			document.getElementById("screen").style.background = color_;
+		},
+
+		set backgroundPicture(image) 
+		{
+			document.getElementById("screen").style.backgroundImage = image;
+		},
+
+		set screenHMTL (html) 
+		{
+			document.getElementById("screen").innerHTML = html;
+		},
+
 		get panic() {/* Calls system panic. */
+		},
+
+		set pause(value)
+		{
+			setTimeout(function() { value.exe },value.timeout);
 		},
 
 		// Info funcitons
@@ -249,43 +90,55 @@ let kernel = (function() {
 })();
 function power() { kernel.power; } // Binds power
 
-/*******************
- iJD Display Driver
-*******************/
-let display = (function() {
+/*************************************
+    	U s e r   S p a c e
+*************************************/
+
+/***************************
+ iJD Console 6 UI Controller
+****************************/
+let usr_interace = (function() {
 
 	// Private Variables
-	let _background = "", // Path to background.
-		_boot_bg = ""
+	    // Boot time spacing
+	let BOOT_STAGE1 = 200,
+		BOOT_STAGE2 = 1000,
+		BOOT_STAGE3 = 4800,
+		BOOT_STAGE4 = 5000,
+
+		// Desktop time spacing
+		DESKTOP_STAGE1 = 500,
+		DESKTOP_STAGE2 = 1200;
 
 	return {
 
-		version : '0.1', // Display Driver Version
+		version : '0.1', //@ Display Driver Version
 
 		get boot() { // What to show while booting up.
-			setTimeout(function() {document.getElementById("screen").style.background = '#fff';},200);
-			setTimeout(function() {document.getElementById("screen").style.backgroundImage = 'url("sys/styles/boot.jpg")';},1000);
-			setTimeout(function() {document.getElementById("screen").style.backgroundImage = 'none';},4800);
-			setTimeout(function() {document.getElementById("screen").style.background = '#fff';},4800);
-			setTimeout(function() {document.getElementById("screen").style.backgroundImage = 'url("sys/styles/wallpaper_blur.jpg")';},5000);
-			setTimeout(function() {document.getElementById("screen").innerHTML = '<div id="login-title">Welcome.</div><input class="welcome-input" type="text" placeholder="Enter username."><input class="welcome-input" type="password" placeholder="Enter password."><button onclick="sys.login;" id="login-button">Login <i class="fas fa-sign-in-alt"></i></button>'},5000);
+			kernel.pause = { exe: "kernel.backColor = '#fff'", timeout: BOOT_STAGE1};
+			setTimeout(function() {kernel.backgroundPicture = 'url("sys/styles/boot.jpg")';},BOOT_STAGE2);
+			setTimeout(function() {kernel.backgroundPicture = 'none';},BOOT_STAGE3);
+			setTimeout(function() {kernel.backColor = '#fff';},BOOT_STAGE3);
+			setTimeout(function() {kernel.backgroundPicture = 'url("sys/styles/wallpaper_blur.jpg")';},BOOT_STAGE4);
+			setTimeout(function() {kernel.screenHMTL = '<div id="login-title">Welcome.</div><input class="welcome-input" type="text" placeholder="Enter username."><input class="welcome-input" type="password" placeholder="Enter password."><button onclick="sys.login;" id="login-button">Login <i class="fas fa-sign-in-alt"></i></button>';},BOOT_STAGE4);
 		},
 
-		get clear() { // Clears the screen for shutdown
-			document.getElementById("screen").innerHTML = '';
-			document.getElementById("screen").style.backgroundImage = 'none';
-			document.getElementById("screen").style.background = '#000';
+		get clear() { //@ Clears the screen for shutdown
+			kernel.screenHMTL = '';
+			kernel.backgroundPicture = 'none';
+			kernel.backColor = '#000';
 
 		},
 
-		get desktop() { // Show the desktop
-			document.getElementById("screen").innerHTML = '';
-			setTimeout(function() {document.getElementById("screen").style.background = '#fff';},500);
-			setTimeout(function() {document.getElementById("screen").innerHTML = '<div id="top-bar"><a id="info"><i class="fas fa-info"></i></a><span class="mono"> consoleOS 6</span><a id="time"></a></div>'},1200);
-			setTimeout(function() {document.getElementById("screen").style.backgroundImage = 'url("sys/styles/wallpaper.jpg")';time();},1200);
+		get desktop() { //@ Show the desktop
+			setTimeout(function() {kernel.backColor = '#fff';},DESKTOP_STAGE1);
+			setTimeout(function() {kernel.screenHTML = '';},DESKTOP_STAGE1);
+			alert("cealred?")
+			setTimeout(function() {kernel.screenHMTL = '<div id="top-bar"><a id="info"><i class="fas fa-info"></i></a><span class="mono"> consoleOS 6</span><a id="time"></a></div>';},DESKTOP_STAGE2);
+			setTimeout(function() {kernel.backgroundPicture = 'url("sys/styles/wallpaper.jpg")';time();},DESKTOP_STAGE2);
 		},
 
-		date() {
+		date() { //@ Logic for getting date information
 			var date=new Date().getDay();
 			switch(date)
 			{
@@ -320,7 +173,7 @@ let display = (function() {
 			consoleWrite("It is "+date+"; "+month + "/" + day + "/" + year+".");
 		},
 
-		get getTime() {
+		get getTime() { //@ Logic for getting time information
 			var today = new Date();
 		    var h = today.getHours();
 		    var m = today.getMinutes();
@@ -334,36 +187,61 @@ let display = (function() {
 	}
 })(); 
 
+
 let sys = (function() {
 	// Private Variables
 
 	return {
 		get login() {
-			display.desktop; // Load in desktop.
+			usr_interace.desktop; // Load in desktop.
+		},
+
+		get boot() {
+			usr_interace.boot;
+		},
+
+		get clear() {
+			usr_interace.clear;
 		}
 	}
 	
 })();
 
+
+/******************
+ Misc Funcitons 
+ *****************/
+
+/* Time Controller
+#      Controls the time refresh. Recursive function. 
+*/
+var TIME_DELAY = 500; // 500 ms clock refresh rate
 function time() {
-    document.getElementById('time').innerHTML = display.getTime;
+
+    document.getElementById('time').innerHTML = usr_interace.getTime;
+
     if (kernel.powerSts)
-    	setTimeout(time, 500);
+    {
+    	setTimeout(time, TIME_DELAY);
+    }
  	else 
+ 	{
 		document.getElementById("time").innerHTML = '';
+ 	}
 }
 
 
 /* Keydown Event
-   Listener event that runs whenver a key is pressed to check if an action 
+    Listener event that runs whenver a key is pressed to check if an action 
    	needs to be performed.
 */
+var ENTER_KEY = 13;
 document.onkeydown = function(evt) {
 	evt = evt || window.event;
 	
 	var isEnter = false;
 	if ("key" in evt) { isEnter = (evt.key == "Enter");
-	} else { isEnter = (evt.keyCode == 13); }
+	} else { isEnter = (evt.keyCode == ENTER_KEY); }
 	if (isEnter) {
 		power();
 	}
